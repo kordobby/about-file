8c127eee258924239cdace1783d812be485301c9
lxxtrue <poordobby@naver.com>
2023-07-15 14:05:08 +0900

    add. file input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import styled from "styled-components";
 
 function App() {
-  return <div className="App"></div>;
+  const handleFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("target Value 👉🏻", event.target.value); // C:\fakepath\Group 208.jpg
+    console.log("target File 👉🏻", event.target.files); // File object...
+    const file = event.target.files;
+    console.log("File Type 👉🏻", typeof file); // object
+  };
+
+  return (
+    <Layout>
+      <Input type="file" id="imgFile" onChange={handleFile} />
+    </Layout>
+  );
 }
 
 export default App;
+
+const Layout = styled.section`
+  width: 100vh;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  /* width: 500px;
+  height: 50px;
+  border-radius: 20px; */
+`;
